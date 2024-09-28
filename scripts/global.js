8e15fcbc15f37d1b9a98deb4e96709d65225bc5f
Bubbletea14 <61301060+Bubbletea14@users.noreply.github.com>
2024-09-28 04:18:03 -0400

    Add function to linkedIn ul
    Make LinkedIn ul clickable and functional in the side nav bar

---
+++ scripts/global.js
@@ -0,0 +1,10 @@
+
+// addEventListener("DOMContentLoaded", (event) => {});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("linkedIn").addEventListener("click", function () {
+        window.location.href = "https://www.linkedin.com/in/liyuan-jin";
+    });
+
+});
